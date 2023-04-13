895ff947ed77e786dca7e78bcce6c7ea086e2e11
Ospina <mateo.ospina@softwareone.com>
2023-04-13 13:28:51 -0500

    the functionality of the footer is added to indicate how many todoItems exist

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TodolistItemsServiceService } from 'src/app/services/todolist-items-service.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
 
+  todoItemsCounter: number = 0;
+  todoItemsCounterDescription: string = '';
+
+  constructor(private todolistItemsServiceService:TodolistItemsServiceService) { }
+  ngOnInit(): void {
+    this.todolistItemsServiceService.todoListFromStorage$.subscribe((v) => {
+      this.todoItemsCounter = v.length;
+      this.todoItemsCounterDescription =
+        this.todoItemsCounter == 1
+          ? '1 item left'
+          : `${this.todoItemsCounter} items left`;
+    });
+  }
+
+
+  //<strong>0</strong> item left
 }
